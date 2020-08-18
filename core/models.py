7c3d1220a96b9f6600a352d706e92ff7c06921e8
Paulo Soares <soaresprogramador@gmail.com>
2020-08-17 23:45:34 -0300

    Criada tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -11,9 +11,11 @@ class Evento(models.Model):
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
     class Meta:
         db_table = 'evento'
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M horas')
